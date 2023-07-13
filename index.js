a54d9db552841c419f6f89f0c2bf1f604580331c
plsgivemeachane <100744636+plsgivemeachane@users.noreply.github.com>
2023-07-13 08:32:56 +0000

    Account image changing

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express")
-
+const FormData = require('form-data');
+const multer = require('multer');
 const app = express()
+
+// SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+var upload = multer({ storage: storage })
 
 app.use(express.static(__dirname + "/static"))
 
@@ -18,6 +31,17 @@ app.get("/login", (req, res) => {
 
 app.get("/friends", (req, res) => {
     res.sendFile(__dirname + "/friends.html")
+})
+
+app.post('/upload', upload.single('myImage'), (req, res, next) => {
+    const file = req.file
+    console.log(file)
+    if (!file) {
+        const error = new Error('Please upload a file')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(file)
 })
 
 app.get("/register", (req, res) => {
